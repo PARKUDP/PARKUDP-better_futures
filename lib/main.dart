7c7acda0e94986909e69bcb1fc4214f53bf6d1db
e225752 <e225752@ie.u-ryukyu.ac.jp>
2024-04-28 14:08:37 +0900

    解決

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/screen/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'screen/total.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: "To Do List", home: TotalPage());
+    return const MaterialApp(
+      title: "To Do List",
+      home: Loginpage(),
+    );
   }
 }
--- lib/screen/signin.dart
+++ lib/screen/signin.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'login.dart';
 
 class SigninPage extends StatefulWidget {
   const SigninPage({super.key});
@@ -10,37 +11,42 @@ class SigninPage extends StatefulWidget {
 }
 
 class _SigninPageState extends State<SigninPage> {
-  // 入力されたユーザーの名前
   String newUser = "";
-  // 入力されたメールアドレス
   String newUserEmail = "";
-  // 入力されたパスワード
   String newUserPassword = "";
-  // 登録・ログインに関する情報を表示
   String infoText = "";
-  // FireStoreにユーザー情報を登録する関数を定義
   CollectionReference users = FirebaseFirestore.instance.collection('users');
-  Future<void> Register() {
-    // 上で定義したメンバ変数を格納すると、usersコレクションに、
-    // メールアドレスとパスワードも保存できる。
-    return users
-        .add({
-          'name': newUser,
-          'email': newUserEmail,
-          'password': newUserPassword
-        })
-        .then((value) => print("新規登録に成功"))
-        .catchError((error) => print("新規登録に失敗しました!: $error"));
+
+  // Firestoreに名前とメールアドレスを保存する関数
+  Future<void> registerUserInFirestore(User? user) async {
+    if (user != null) {
+      return users.doc(user.uid).set({
+        'name': newUser,
+        'email': newUserEmail,
+      });
+    } else {
+      throw Exception('ユーザー作成エラー');
+    }
   }
 
-  // メールアドレスとパスワードを登録する関数を定義
-  final FirebaseAuth auth = FirebaseAuth.instance;
-  Future<void> createAuth() async {
-    UserCredential result = await auth.createUserWithEmailAndPassword(
-      email: newUserEmail,
-      password: newUserPassword,
-    );
-    final User user = result.user!;
+  // ユーザー登録処理を統合
+  Future<void> register() async {
+    try {
+      UserCredential result =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: newUserEmail,
+        password: newUserPassword,
+      );
+      await registerUserInFirestore(result.user);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Loginpage()),
+      );
+    } catch (e) {
+      setState(() {
+        infoText = "登録失敗: ${e.toString()}";
+      });
+    }
   }
 
   @override
@@ -50,8 +56,6 @@ class _SigninPageState extends State<SigninPage> {
         title: const Text('ユーザー登録'),
         backgroundColor: const Color.fromARGB(255, 229, 255, 0),
       ),
-      // キーボードで隠れて、黄色エラーが出るので
-      // SingleChildScrollViewで、Centerウイジットをラップする
       body: SingleChildScrollView(
         child: Center(
           child: Container(
@@ -59,56 +63,23 @@ class _SigninPageState extends State<SigninPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                const SizedBox(height: 40),
                 TextFormField(
-                  // テキスト入力のラベルを設定
                   decoration: const InputDecoration(labelText: "ユーザー名"),
-                  onChanged: (String value) {
-                    setState(() {
-                      newUser = value;
-                    });
-                  },
+                  onChanged: (value) => setState(() => newUser = value),
                 ),
-                const SizedBox(height: 8),
                 TextFormField(
-                  // テキスト入力のラベルを設定
                   decoration: const InputDecoration(labelText: "メールアドレス"),
-                  onChanged: (String value) {
-                    setState(() {
-                      newUserEmail = value;
-                    });
-                  },
+                  onChanged: (value) => setState(() => newUserEmail = value),
                 ),
-                const SizedBox(height: 8),
                 TextFormField(
-                  decoration: const InputDecoration(labelText: "パスワード（６文字以上）"),
-                  // パスワードが見えないようにする
+                  decoration: const InputDecoration(labelText: "パスワード（6文字以上）"),
                   obscureText: true,
-                  onChanged: (String value) {
-                    setState(() {
-                      newUserPassword = value;
-                    });
-                  },
+                  onChanged: (value) => setState(() => newUserPassword = value),
                 ),
-                const SizedBox(height: 8),
                 ElevatedButton(
-                  onPressed: () async {
-                    try {
-                      // FireStoreにユーザー情報を登録する関数
-                      Register();
-                      // メール/パスワードでユーザー登録
-                      createAuth();
-                      // 登録したユーザー情報
-                    } catch (e) {
-                      // 登録に失敗した場合
-                      setState(() {
-                        infoText = "登録NG:${e.toString()}";
-                      });
-                    }
-                  },
+                  onPressed: register,
                   child: const Text("ユーザー登録"),
                 ),
-                const SizedBox(height: 8),
                 Text(infoText)
               ],
             ),
